f05c3c5d7da45fa15d985812eb4bd44928bdd67e
FernandoBerganza <fberganzac@miumg.edu.gt>
2024-02-17 12:49:20 -0600

    Fernando Berganza
    Los cambios de Fernando

---
+++ Suma Fernando Berganza.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	int x=4; // declaramos e inicializamos una variable entera 
+	int y=1;
+	
+	int suma= x+y; // realizamos la operacion 
+	
+	//imorimimos en pantalla el resultado 
+	cout << "La suma es: "<<suma<< endl;
+	return 0;
+}
